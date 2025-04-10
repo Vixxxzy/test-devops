cb45ecfe053cb17cbec3ece74186a4a2898209f7
Vic <143060326+Vixxxzy@users.noreply.github.com>
2025-04-10 15:08:55 +0800

    update title form and submit button

--- app/view/login/index.php
+++ app/view/login/index.php
@@ -18,7 +18,7 @@
 <body oncontextmenu='return false' class='snippet-body'>
 <div class="container wrapper-center">
 	<div class="wrapper bg-white">
-		<div class="h2 text-center">Vichers</div>
+		<div class="h2 text-center">Vichers Rory</div>
 		<form action="<?= APP_PATH;?>/login/process" method="post" class="pt-3">
 			<div class="form-group py-1 pb-2">
 				<div class="input-field">
@@ -54,7 +54,7 @@
                 }; 
             
             ?>
-			<button type="submit" class="btn btn-block text-center mt-3">Log in</button>
+			<button type="submit" class="btn btn-block text-center mt-3">Login</button>
 		</form>
 	</div>
 </div>
