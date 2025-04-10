70c83ea1c05ea22b39dfc7b12f143fdf6705f61e
Vic <143060326+Vixxxzy@users.noreply.github.com>
2025-04-10 08:03:29 +0800

    adding app and public

---
+++ app/app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+	// define path constants
+    define("APP_PATH", "http://103.23.199.190/unkpresent/Devops/public");
+
+    // define database config
+    define("HOST_DB", "localhost");
+    define("NAME_DB", "db_attendance");
+    define("USER_DB", "root");
+    define("PASS_DB", "Jack78huntter.");
+	
+	
+?>
